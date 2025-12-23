774ecf066bcf37507070122a51ee886aef4bada3
Suraj Sakhare <122685473+SurajSakhare100@users.noreply.github.com>
2025-12-23 17:31:41 +0530

    Update Resume.ts

--- config/Resume.ts
+++ config/Resume.ts
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/11QCUrCi41DbO2mWA_AcpWPqM4ec8Jicgpreview',
+  url: 'https://drive.google.com/file/d/11QCUrCi41DbO2mWA_AcpWPqM4ec8Jicg/preview',
 };
